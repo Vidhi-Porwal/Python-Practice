60c1ab81a2182d875045597bccefc9f010be7d78
Vidhi-Porwal <vidhi.porwal@gmail.com>
2024-05-30 18:01:05 +0530

    understanding using arithmetic operators

---
+++ arithmatic operators.py
@@ -0,0 +1,18 @@
+"""Write a python program/script to perform the following arithematic
+operation over 2 user defined integer & print results in different lines """
+"""var_a = 5
+var_b = 6"""
+
+var_a = int(input("Enter Value :"))
+var_b = int(input("Enter Value :"))
+
+result_2= var_a- var_b
+print(result_2)
+result_3= var_a* var_b
+print(result_3)
+result_4= var_a// var_b
+print(result_4)
+result_5= var_a% var_b
+print(result_5)
+result_1= var_a+ var_b
+print( "Sum is :" + str(result_1))
